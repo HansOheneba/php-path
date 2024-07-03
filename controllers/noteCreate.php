a606b5d57559e4c4d61a02053a1fe0cbc1771a96
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2024-07-03 07:34:17 +0000

    Form input Validator logic

--- controllers/noteCreate.php
+++ controllers/noteCreate.php
@@ -6,12 +6,26 @@ $db = new Database($dataConfig['database']);
 
 $heading = "Create a Note";
 
+
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+$errors=[];
+
+if( strlen($_POST['body']) === 0 ){
+    $errors['body'] = "Oh dear... Seems your note does not have a body";
+}
+if( strlen($_POST['body']) > 1000 ){
+    $errors['body'] = "Oh dear.. Notes can not be longer than 1,000 characters";
+}
+if(empty($errors)){
     $db->query('INSERT INTO notes(body, userID) VALUES(:body, :userID)', 
 [
     'body' => $_POST['body'],
     'userID' => 1
 ]);
+}
+    
+
 }
 
 require('views/noteCreate.view.php');
