8cf01765e70e70ae0eea568167e9827b6be7cd07
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2024-07-05 06:37:43 +0000

    Notes namign conventions updated

--- routes.php
+++ routes.php
@@ -5,7 +5,7 @@ return [
     '/about'=>'controllers/about.php',
     '/contact'=>'controllers/contact.php',
     '/mission'=>'controllers/mission.php',
-    '/notes'=>'controllers/notes.php',
-    '/notes/create'=>'controllers/noteCreate.php',
-    '/note'=>'controllers/note.php',
+    '/notes'=>'controllers/notes/index.php',
+    '/notes/create'=>'controllers/notes/create.php',
+    '/note'=>'controllers/notes/show.php',
     ];
