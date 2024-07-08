26bb282526ced6431f9a98f62e3d7aa8cf54c401
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2024-07-08 05:46:29 +0000

    Added feature to delete note

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -7,6 +7,27 @@ $db = new Database($dataConfig['database']);
 
 $currentUserID = 1;
 
+
+
+if($_SERVER['REQUEST_METHOD']==='POST'){
+
+    $note = $db->query("select * from notes where id = :id", [ 
+        'id' => $_GET['id']
+        ])->fetchOrAbort();
+    
+    
+    authorize($note['userID'] === $currentUserID);
+
+
+    $note = $db->query("delete from notes where id = :id", [ 
+        'id' => $_GET['id']
+        ])->fetchAll();
+
+        header('location: /notes');
+        die();
+
+}else{
+
 $note = $db->query("select * from notes where id = :id", [ 
     'id' => $_GET['id']
     ])->fetchOrAbort();
@@ -19,7 +40,10 @@ authorize($note['userID'] === $currentUserID);
 
 
 
+
+
 view("notes/show.view.php", [
     'heading' => 'Note',
     'note' => $note
 ]);
+}
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -2,25 +2,25 @@
 <?php require base_path('views/partials/nav.php') ?>
 <?php require base_path('views/partials/banner.php') ?>
 
-
-  
-    <main class="px-5">
-      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 ">
-      <a href="/notes"
-        class="inline-flex items-center border border-black px-3 py-1.5 rounded-md text-black hover:bg-gray-50 mb-6">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16l-4-4m0 0l4-4m-4 4h18">
-            </path>
-        </svg>
-        <span class="ml-1 font-bold text-lg">Back</span>
+<main class="container mx-auto flex-grow py-8">
+  <div class="bg-white shadow-lg rounded-lg p-6 relative">
+    <a href="/notes" class="inline-flex items-center border border-black px-2 py-1 rounded-md text-black hover:bg-gray-50 mb-6">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16l-4-4m0 0l4-4m-4 4h18">
+        </path>
+      </svg>
+      <span class="ml-1 font-bold text-sm">Back</span>
     </a>
-       <p class="px-10"><?= htmlspecialchars($note['body']) ?></p>
-      </div>
-    </main>
+    <p class="text-gray-700"><?= htmlspecialchars($note['body']) ?></p>
+<form action="" method="post">
+  <input type="hidden" name="id" value="<?=$note['id']?>">
+<button class="absolute top-4 right-4 bg-red-500 text-sm text-white px-2 py-1 rounded hover:bg-red-600 transition duration-300">Delete</button>
+</form>
+  </div>
+</main>
 
 
-    
-    <?php require base_path('views/partials/foot.php') ?>
+
+<?php require base_path('views/partials/foot.php') ?>
+
 </html>
-
-
